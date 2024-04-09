419c253bf14d2940271dfdb9e42cd9fc281f8ef6
aoiumi94 <161325982+aoiumi94@users.noreply.github.com>
2024-04-09 13:36:34 +0900

    Add files via upload

---
+++ ATextBox.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Test5
+{
+    /// <summary>
+    /// ATextBox.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ATextBox : UserControl
+    {
+            public ATextBox()
+        {
+            InitializeComponent();
+
+            //this.DataContext = this;
+            entire.DataContext = this;
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text",
+                                        typeof(string),
+                                        typeof(ATextBox),
+                                        new FrameworkPropertyMetadata(string.Empty));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test5
+{
+    public class Info
+    {
+        public string Title { get; set; } = "";
+        public string Name { get; set; } = "";
+    }
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Info> infos = new ObservableCollection<Info>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+
+
+            infos.Add(new Info { Title = "title1", Name = "name1" });
+            infos.Add(new Info { Title = "title2", Name = "name2" });
+
+            listBox.ItemsSource = infos;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = infos[0].Title;
+        }
+    }
+}
